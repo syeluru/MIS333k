41d72c96b222aebe434c15419fd2ecdb60fadaa7
saiyeluru <syeluru96@gmail.com>
2016-09-26 03:15:23 -0500

    created customer model and appdbcontext config file in dal

---
+++ HW/Yeluru_Sai_HW3/Yeluru_Sai_HW3/DAL/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Yeluru_Sai_HW3.Models;
+using System.Data.Entity;
+
+namespace Yeluru_Sai_HW3.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("MyDBConnection") { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+
+    }
+}
---
+++ HW/Yeluru_Sai_HW3/Yeluru_Sai_HW3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Yeluru_Sai_HW3.MvcApplication" Language="C#" %>
